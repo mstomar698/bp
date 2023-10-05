7a75c7fec5598b044881753f4a74b5a531b09149
mstomar698 <tomarm698@gmail.com>
2023-10-05 15:08:20 +0530

    users andf profile screen with tehre protected routes

--- client/src/AllRoutes.tsx
+++ client/src/AllRoutes.tsx
@@ -2,12 +2,50 @@ import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import SigninScreen from './screens/SiginScreen';
 import SignupScreen from './screens/SignupScree';
+import AdminRoute from './components/adminRoute';
+import UserEditScreen from './screens/userEditScreen';
+import UserListScreen from './screens/userListScreen';
+import DashboardScreen from './screens/dashboardScreen';
+import ProtectedRoute from './components/protectedRoute';
+import ProfileScreen from './screens/profileScreen';
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/signin" element={<SigninScreen />} />
       <Route path="/signup" element={<SignupScreen />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfileScreen />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <AdminRoute>
+            <DashboardScreen />
+          </AdminRoute>
+        }
+      ></Route>
+      <Route
+        path="/admin/users"
+        element={
+          <AdminRoute>
+            <UserListScreen />
+          </AdminRoute>
+        }
+      ></Route>
+      <Route
+        path="/admin/user/:id"
+        element={
+          <AdminRoute>
+            <UserEditScreen />
+          </AdminRoute>
+        }
+      ></Route>
       <Route path="/" element={<HomeScreen />} />
     </Routes>
   );
