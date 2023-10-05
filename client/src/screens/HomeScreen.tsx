4f7cf9506ba3c6bbb4b4a12056428abd0079ef22
mstomar698 <tomarm698@gmail.com>
2023-10-05 15:08:20 +0530

    users andf profile screen with tehre protected routes

--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,15 +1,19 @@
 import { Store } from '../store';
 import { useContext } from 'react';
+import Navbar from '../components/navbar';
 
 const HomeScreen = () => {
   const { state } = useContext(Store);
   const { userInfo } = state;
   console.log(userInfo);
   return (
-    <div className="text-green-300 min-h-screen flex flex-grow justify-center items-center">
-      <div>
-        HomeScreen <br />
-        {userInfo ? <>Authenticated</> : <>UnAuthenticated</>}
+    <div>
+      <Navbar />
+      <div className="text-green-300 min-h-screen flex flex-grow justify-center items-center">
+        <div>
+          HomeScreen <br />
+          {userInfo ? <>Authenticated</> : <>UnAuthenticated</>}
+        </div>
       </div>
     </div>
   );
