9dfc9291c52293d54bf589d9ecec00b0f89075ed
mstomar698 <tomarm698@gmail.com>
2023-10-07 17:10:19 +0530

    book card screena dn other routes

--- server/routes/books.ts
+++ server/routes/books.ts
@@ -90,6 +90,20 @@ bookRouter.patch('/edit/:id', isAuth, async (req: Request, res: Response) => {
   }
 });
 
+bookRouter.get('/:id', isAuth, async (req: Request, res: Response) => {
+  const bookId = req.params.id;
+  console.log(`Fetching book with ID: ${bookId}`);
+  const book = await Book.findById(bookId);
+  if (book) {
+    console.log(`Found book: ${book}`);
+    res.send(book);
+  } else {
+    console.log(`Book Not Found with ID: ${bookId}`);
+    res.status(404).send({ message: 'Book Not Found' });
+  }
+});
+
+
 
 // 5. Delete a book (Requires authentication, admin, or the user who published the book)
 bookRouter.delete('/delete/:id', isAuth, async (req: Request, res: Response) => {
